7c768cb7e54aa8246abe9d0d8bbe5afa7e83bb1c
Anurag Nanda <anurragnanda@anurrags-imac.local>
2016-02-03 00:07:09 -0800

    FirstCommit

--- Yelp/AppDelegate.swift
+++ Yelp/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Yelp
 //
-//  Created by Anurag Nanda on 2/2/16.
-//  Copyright © 2016 Anurag Nanda. All rights reserved.
+//  Created by Timothy Lee on 9/19/14.
+//  Copyright (c) 2014 Timothy Lee. All rights reserved.
 //
 
 import UIKit
